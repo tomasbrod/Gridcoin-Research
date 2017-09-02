51ff55d9937516ba7312d5e9e2baa41904764f63
Tomáš Brada <tomasbrod@azet.sk>
2017-09-02 21:27:37 +0200

    Work on rpc_writeplotdiff.

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -4663,7 +4663,8 @@ json_spirit::Value rpc_getblockstats(const json_spirit::Array& params, bool fHel
 {
     if(fHelp || params.size() < 1 || params.size() > 3 )
         throw runtime_error(
-            "getblockstats mode [startheight [endheight]]\n"
+            "getblockstats 0 [startheight [endheight]]\n"
+            "getblockstats 1 [samples [endheight]]\n"
             "Show stats on what wallets and cpids staked recent blocks.\n");
     long mode= cdbl(params[0].get_str(),0);
     (void)mode; //TODO
@@ -4871,15 +4872,20 @@ json_spirit::Value rpc_writeplotdiff(const json_spirit::Array& params, bool fHel
 {
     if(fHelp)
         throw runtime_error(
-            "writeplotdiff TODO\n");
+            "writeplotdiff [maxblocks points [endblock]] \n");
     /* count, high */
-    long highheight= INT_MAX;
-    long maxblocks= 100000;
-    /* count highheight */
+    long endblock= INT_MAX;
+    long maxpoints= 700;
+    int  smoothing= 22;
     if(params.size()>=2)
-        maxblocks= cdbl(params[1].get_str(),0);
+    {
+        long maxblocks= cdbl(params[0].get_str(),0);
+        maxpoints= cdbl(params[1].get_str(),0);
+        smoothing = maxblocks / maxpoints;
+        maxpoints = ceil(maxblocks / (double)smoothing);
+    }
     if(params.size()>=3)
-        highheight= cdbl(params[2].get_str(),0);
+        endblock= cdbl(params[2].get_str(),0);
     CBlockIndex* cur;
     Object result1;
     {
@@ -4887,6 +4893,11 @@ json_spirit::Value rpc_writeplotdiff(const json_spirit::Array& params, bool fHel
         cur= pindexBest;
     }
     int64_t blockcount = 0;
+    long points = 0;
+    long samples = 0;
+    double diffsum = 0;
+    double diffmin = INT_MAX;
+    double diffmax = 0;
     boost::filesystem::path o_path = GetDataDir() / "reports" / ( "diff_" + std::to_string(GetTime()) + ".txt" );
     boost::filesystem::create_directories(o_path.parent_path());
     ofstream Output;
@@ -4894,19 +4905,36 @@ json_spirit::Value rpc_writeplotdiff(const json_spirit::Array& params, bool fHel
     Output.imbue(std::locale::classic());
     Output << std::fixed << std::setprecision(4);
     for( ; (cur
-            &&( blockcount<maxblocks )
+            &&( points<=maxpoints )
         );
         cur= cur->pprev
         )
     {
-        if(cur->nHeight>highheight)
+        if(cur->nHeight>endblock)
             continue;
         blockcount++;
         double diff = GetDifficulty(cur);
-        Output << cur->nHeight << " " << diff << '\n';
+        diffsum+= diff;
+        diffmin=std::min(diffmin,diff);
+        diffmax=std::max(diffmax,diff);
+        samples++;
+        if(samples>=smoothing)
+        {
+            const double avgdiff= diffsum / samples;
+            int midheight = cur->nHeight + (smoothing/2);
+            Output << midheight << " " << avgdiff << " " << diffmin << " " << diffmax << '\n';
+            samples = 0;
+            diffsum = 0;
+            diffmin = INT_MAX;
+            diffmax = 0;
+            points++;
+        }
     }
 
     result1.push_back(Pair("file", o_path.string()));
+    result1.push_back(Pair("points",points));
+    result1.push_back(Pair("smoothing",smoothing));
+    result1.push_back(Pair("blockcount",blockcount));
     Output.close();
     return result1;
 }
